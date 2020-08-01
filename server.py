204b3f4437134351c057792f9128d01ef16c00cf
Tanmay <tnk2121998@yahoo.com>
2020-08-01 13:14:56 +0530

    my portfolio

---
+++ server.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template,request,redirect
+import csv
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/<username>/<int:post_id>')
+def hello_world(username=None,post_id=None):
+    return render_template('index.html',name=username,id=post_id)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_data(data):
+	with open('database.txt',mode='a') as database:
+		email=data["email"]
+		subject=data["subject"]
+		message=data["message"]
+		database.write(f'\n(*) {email}, {subject}, {message}')
+
+def write_to_csv(data):
+	with open('database.csv',newline='',mode='a') as database2:
+		email=data["email"]
+		subject=data["subject"]
+		message=data["message"]
+		csv_writer=csv.writer(database2,delimiter=',',quotechar='"',quoting=csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method =='POST':
+		data=request.form.to_dict()
+		write_to_csv(data)
+		return redirect('/thankyou.html')
+	else:
+		return('Error')
+    # return render_template('login.html', error=error)
